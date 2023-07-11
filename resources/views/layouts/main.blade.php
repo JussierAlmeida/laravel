225eedef9facbae22486134e7b0d1c72967a7a24
JussierAlmeida <127342987+JussierAlmeida@users.noreply.github.com>
2023-07-10 22:26:21 -0300

    configuração. google fonts. bootstrap. css.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,15 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title')</title>
-        
+    <title>@yield('title')</title>
 
+    <!--font do google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;0,900;1,300;1,700;1,900&display=swap" rel="stylesheet">
+       
+    <!--css bootstrep-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+       
+        <!--css aplicação-->
     <link rel="stylesheet" href="/css/styles.css">
     <script>    src="/js/scripts.js"    </script>
-    </head>
+</head>
     <body>
         @yield('content')
         <footer>
