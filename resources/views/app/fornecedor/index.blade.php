2082fb4178cca8482f24857a4e2a2c11e32776d0
JussierAlmeida <127342987+JussierAlmeida@users.noreply.github.com>
2023-07-19 23:36:05 -0300

    realizado teste de confiçoes

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,1 +1,28 @@
 <h1>fornecedor no blade</h1>
+
+@php
+    
+   /* if (condition) {
+        # code...
+    }elseif (condition) {
+        # code...
+    }else {
+        # code...
+    }
+*/
+
+@endphp
+   
+    @if(count($fornecedores)> 0 && count($fornecedores)< 10)
+
+    <h3> Existem alguns fornecedores cadastrados</h3> 
+
+    @elseif(count($fornecedores)> 10)
+
+     <h3> Existem varios fornecedores cadastrados</h3>    
+
+     @else
+     
+     <h3> ainda não existe fornecedores cadastrados</h3> 
+     @endif
+
