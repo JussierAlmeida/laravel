e953f39cdbddba04814e995f3953691ee3cf2393
JussierAlmeida <127342987+JussierAlmeida@users.noreply.github.com>
2023-07-20 23:15:28 -0300

    realiza testes. de isset e empty

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,37 +1,18 @@
 <h1>fornecedor no blade</h1>
 
-@php
-    
-   /* if (condition) {
-        # code...
-    }elseif (condition) {
-        # code...
-    }else {
-        # code...
-    }
-*/
 
-//comparação do if para unless
-    /*if (!<condition>) {
-        # code...
-    }*/
-
-@endphp
-@dd($fornecedores);
-   
-    {{--@unless executa se o resultado for false--}}
-
-
-    {{--@if(count($fornecedores)> 0 && count($fornecedores)< 10)
-
-    <h3> Existem alguns fornecedores cadastrados</h3> 
-
-    @elseif(count($fornecedores)> 10)
-
-     <h3> Existem varios fornecedores cadastrados</h3>    
-
-     @else
-     
-     <h3> ainda não existe fornecedores cadastrados</h3> 
-     @endif
---}}
+    @isset($fornecedores)
+        Fornecedor:{{$fornecedores[0]['nome']}}
+        <br>
+        Status: {{$fornecedores[0]['status']}}
+        <br>
+        @isset($fornecedores[0]['cnpj'])
+            cnpj: {{$fornecedores[0]['cnpj']}}  
+            @empty($fornecedores[0]['cnpj'])
+                - Vazio
+            @endempty
+        @endisset
+        
+    @endisset
+    {{--isset verifica se a variavel existe--}}
+    {{--verifica se a variavel esta vazia: '', 0, 0.0, '0' null false array(), variavel sem valor--}}
