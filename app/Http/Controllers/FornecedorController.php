794241e6aba05be7672f54f891afd356ef5c53cd
JussierAlmeida <127342987+JussierAlmeida@users.noreply.github.com>
2023-07-24 21:42:39 -0300

    cria um teste. for

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,16 +9,26 @@ class FornecedorController extends Controller
     public function index(){
         $fornecedores = [
             0 => [
-                'nome'=>'Fornecedor 1',
-                'status'=>'N',
-                'cnpj'=>''
+                'nome'=>'Natal Computer',
+                'status'=>'Inativo',
+                'cnpj'=>'1513135844321',
+                'ddd' => '11',
+                'telefone' => ' 0000-0000'
             ],
             1 => [
-                'nome'=>'Fornecedor 2',
-                'status'=>'S',
-                'cnpj'=>null
-                
-            ]
+                'nome'=>'Megabity',
+                'status'=>'Ativo',
+                'cnpj'=>null,
+                'ddd' => '85',
+                'telefone' => ' 0000-1111'
+            ],
+            2 => [
+                'nome'=>'Positivo',
+                'status'=>'Inativo',
+                'cnpj'=>'65454313846446',
+                'ddd' => '32',
+                'telefone' => ' 0000-2222'
+            ],
         
         ];
 
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,11 +2,18 @@
 
 
     @isset($fornecedores)
-        Fornecedor:{{$fornecedores[1]['nome']}}
-        <br>
-        Status: {{$fornecedores[1]['status']}}
-        <br>
-        CNPJ: {{ $fornecedores[1]['cnpj']?? 'Dado não preenchido'}}
+        @for ($i = 0; isset($fornecedores[$i]); $i++)
+
+            Fornecedor:{{$fornecedores[$i]['nome']}}
+            <br>
+            Status: {{$fornecedores[$i]['status']}}
+            <br>
+            CNPJ: {{ $fornecedores[$i]['cnpj']?? 'Dado não preenchido'}}
+            <br>
+            TELEFONE: ({{ $fornecedores[$i]['ddd']?? 'Dado não preenchido'}}){{$fornecedores[$i]['telefone']?? ''}}
+            <hr>    
+            
+        @endfor
     @endisset
     {{--isset verifica se a variavel existe--}}
     {{--verifica se a variavel esta vazia: '', 0, 0.0, '0' null false array(), variavel sem valor--}}
