dc78f52e2e7ce940da1e247e4f86ef280b9cb719
JussierAlmeida <127342987+JussierAlmeida@users.noreply.github.com>
2023-07-24 23:25:50 -0300

    cria um teste. função loop/iteration/first last

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -3,16 +3,32 @@
 
     @isset($fornecedores)
            
-    @foreach ($fornecedores as $indece => $fornecedor)
-           
-            Fornecedor:{{$fornecedor['nome']}}
-            <br>
-            Status: {{$fornecedor['status']}}
-            <br>
-             CNPJ: {{ $fornecedor['cnpj']?? 'Dado não preenchido'}}
-            <br>
-            TELEFONE: ({{ $fornecedor['ddd']?? 'Dado não preenchido'}}){{$fornecedor['telefone']?? ''}}
-            <hr>    
-    @endforeach     
+        @forelse ($fornecedores as $indece => $fornecedor)
+                iteração atual: {{$loop->iteration}}
+                <br>
+                Fornecedor:{{$fornecedor['nome']}}
+                <br>
+                Status: {{$fornecedor['status']}}
+                <br>
+                CNPJ: {{ $fornecedor['cnpj']?? 'Dado não preenchido'}}
+                <br>
+                TELEFONE: ({{ $fornecedor['ddd']?? 'Dado não preenchido'}}){{$fornecedor['telefone']?? ''}}
+                <br>
+                @if ($loop->first)
+                 This is the first iteration
+                    
+                @endif
+                <br>
+                @if ($loop->last)
+                  This is the last iteration
+                    <br>
+                    Total de registro: {{$loop->count}}
+                @endif
+                <hr> 
+            
+            @empty
+                Não existe fornecedores cadastrados!!
+        @endforelse
+                    
     @endisset
    
